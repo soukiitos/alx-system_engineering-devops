4cb2f0f6640da8e0330f5bfaa74ace4b8e13fa99
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-23 23:17:18 +0100

    README file, Gather data from an API, Export to CSV

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,8 @@ import sys
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    nameurl = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    nameurl = requests.get(
+            url + "todos", params={"userId": sys.argv[1]}).json()
     completed = [
             td.get("title") for td in nameurl if td.get("completed") is True
             ]
