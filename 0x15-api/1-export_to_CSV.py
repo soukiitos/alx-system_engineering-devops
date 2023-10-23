03695e8ff9d4fcf30190046ab4ffac73430839d3
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-23 23:26:15 +0100

    README file, Gather data from an API, Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,17 +6,17 @@ import csv
 
 
 if __name__ == '__main__':
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    user_name = user.get("user_name")
-    nameurl = requests.get(
-            url + "todos", params={"userId": sys.argv[1]}).json()
-    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csv_file:
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    user_name = user.get("username")
+    nameurl = requests.get(url + "todos", params={"userId": user_id}).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for todo in nameurl:
-            writer.writerow({
-                sys.argv[1],
+            writer.writerow([
+                user_id,
                 user_name,
                 todo.get("completed"),
                 todo.get("title")
-                })
+                ])
