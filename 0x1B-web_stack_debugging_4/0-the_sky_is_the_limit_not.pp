a480663381cf2e4a4e6100783cadf2d7f1b5e163
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-11-16 00:54:55 +0100

    The first task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# increase the amount of traffic
+
+exec { 'fix-fr-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d'
+}
