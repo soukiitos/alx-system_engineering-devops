e2cdb9f8913043e0b38e5ab172ac8c75bb98ca56
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-11-16 17:03:44 +0100

    Sky is the limit, let s bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 
 exec { 'fix-fr-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
 } ->
 
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d'
+  path    => '/etc/init.d',
 }
