6d30ba9d1b628ae428c28dca9a08bd94d7f5e4bc
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-11-16 17:39:49 +0100

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,12 @@
 # Enable the user to login
 
+# Increase hard file limit for user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton haed/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
+# Increase soft file limit for user
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
