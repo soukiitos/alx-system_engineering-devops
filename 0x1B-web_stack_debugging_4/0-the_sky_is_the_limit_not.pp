bb9ba166ca19ab7564912624cc92a8eccc48e11c
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-11-16 17:25:01 +0100

    Sky is the limit, let s bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Increases the amount of traffic an Nginx server can handle.
+# increase the amount of traffic
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
+# Increase the ULIMIT
+exec { 'fix-for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx
+# Restart nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d'
 }
